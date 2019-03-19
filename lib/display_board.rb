519cfaa1e2f9ee218aabad5b8c6c400ec0d2320c
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-03-19 17:37:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
+<<<<<<< HEAD
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -10,3 +11,15 @@ end
 
 
 display_board([" "," "," "," "," "," "," "," "," "])
+=======
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+board = ["X"," "," "," ","X"," "," "," ","X"]
+
+display_board(board)
+>>>>>>> 4facda373e37593a86c6aa3da8bedef5f97a44ff
